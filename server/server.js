de7f364a7fae92a04dc443edea0d89bdd9c4a397
Alec Sands <sandsalecv@gmail.com>
2017-06-30 16:24:48 -0500

    jquery sourced

--- server/server.js
+++ server/server.js
@@ -5,3 +5,12 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 var path = require("path");
+
+app.get('/*', function(req, res) {
+  var file = req.params[0] || "/views/index.html";
+  res.sendFile(path.join(__dirname, "/public/", file));
+});
+
+app.listen(5000, function() {
+  console.log('listening on the port 5000');
+});

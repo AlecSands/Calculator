fbbdbcfc5835d59ec437d371ca011991f48edcb4
Alec Sands <sandsalecv@gmail.com>
2017-06-30 17:01:13 -0500

    addition button functionality works.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,23 @@
+var answer;
+
 $('document').ready(function() {
   console.log('jquery sourced');
+  $('#add').on('click', function() {
+    var numberOne = $('#numberOne').val();
+    var numberTwo = $('#numberTwo').val();
+    console.log(numberOne + "" + numberTwo);
+    $.ajax({
+      type: 'POST',
+      url: '/add',
+      data: {
+        numberOne: numberOne,
+        numberTwo: numberTwo
+      },
+      success: function(response) {
+        answer = parseInt(response);
+        console.log(answer);
+        $('#answer').text(Number(answer));
+      }
+    });
+  });
 });
--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var path = require("path");
 
+var answer;
+
+app.post('/add', function(req, res) {
+  request = req.body;
+  console.log(request);
+  answer = parseInt(request.numberOne) + parseInt(request.numberTwo);
+  res.send("" + answer);
+});
+
 app.get('/*', function(req, res) {
   var file = req.params[0] || "/views/index.html";
   res.sendFile(path.join(__dirname, "/public/", file));
